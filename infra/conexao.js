e141d4ce50d2bfe9df5b56247c45a9504b15ea65
omarxlima <marxmaciel@gmail.com>
2025-10-15 11:46:53 -0300

    feat: conexao com banco de dados

---
+++ infra/conexao.js
@@ -0,0 +1,12 @@
+import mysql from 'mysql';
+
+const conexao = mysql.createConnection({
+    host: 'localhost',  
+    port: 3306,
+    user: 'root',
+    password: 'NovaSenha123!',
+    database: 'bd_copa'
+});
+
+
+export default conexao;
--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 import app from './src/app.js'
 
+import conexao from './infra/conexao.js'
+
 const PORT = 3000
 
-app.listen(PORT, () => {
-  console.log(`Example app listening on PORT http://localhost:${PORT}`)
-})
+//fazer a conexçao com o banco de dados
+conexao.connect((erro) => {
+  if (erro) {
+    console.log(erro);
+  } else {
+    console.log('Conexão com o banco de dados realizada com sucesso!');
+    app.listen(PORT, () => {
+      console.log(`Example app listening on PORT http://localhost:${PORT}`)
+    })
+  }
+});
+
